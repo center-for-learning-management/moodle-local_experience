a49a8950fe296319aba9ff09b79f4ea75432dc40
Robert Schrenk <robert@schrenk.cc>
2020-08-21 00:04:42 +0200

    close trigger wrapper

--- lang/de/local_experience.php
+++ lang/de/local_experience.php
@@ -26,7 +26,7 @@ $string['privacy:metadata'] = 'Dieses Plugin speichert keine personenbezogenen D
 
 $string['access_denied'] = 'Zugriff nicht gestattet!';
 $string['advanced_options'] = 'Fortgeschrittener Modus';
-$string['advanced_options:description'] = 'Manche Elemente dieser Seite werden im vereinfachten Modus ausgeblendet. Alle Möglichkeiten stehen Ihnen im fortgeschrittenen Modus zur Verfügung!';
+$string['advanced_options:description'] = 'Manche Elemente dieser Seite werden im vereinfachten Modus ausgeblendet!';
 $string['attachlevelselectto'] = 'Level-Auswahl hinzufügen zu';
 $string['attachlevelselectto:description'] = 'Geben Sie zeilenweise alle HTML-Container mit CSS-Regeln an, zu denen die Level-Auswahl hinzugefügt werden soll. Sie können mittels Parametern angeben, ob das Steuerelement an den Anfang oder das Ende eingefügt werden soll, bzw. ob ein Textlabel angezeigt wird.<br /><br /><i>CSS-Selektor|prepend oder append|true oder false';
 
