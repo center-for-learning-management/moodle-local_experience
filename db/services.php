593a1bd77b8c9c23b75ef7e1fc010fc99ed5872e
GGeorggg <19341320+GGeorggg@users.noreply.github.com>
2026-08-09 23:33:48 +0000

    CI Fixes

--- db/services.php
+++ db/services.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
 /**
  * @package    local_experience
  * @copyright  2020 Center for Learning Management (http://www.lernmanagement.at)
@@ -9,21 +24,21 @@
 defined('MOODLE_INTERNAL') || die;
 
 // We define the web service functions to install.
-$functions = array(
-    'local_experience_injecttext' => array(
+$functions = [
+    'local_experience_injecttext' => [
         'classname' => 'local_experience_external',
         'methodname' => 'injecttext',
         'classpath' => 'local/experience/externallib.php',
         'description' => 'Retrieves an injecttext.',
         'type' => 'read',
         'ajax' => 1,
-    ),
-    'local_experience_keycode' => array(
+    ],
+    'local_experience_keycode' => [
         'classname' => 'local_experience_external',
         'methodname' => 'keycode',
         'classpath' => 'local/experience/externallib.php',
         'description' => 'Performs an action upon keycode press.',
         'type' => 'read',
         'ajax' => 1,
-    ),
-);
+    ],
+];
